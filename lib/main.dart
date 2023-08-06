0fb78d959fc990e905595c4e82c34615f50e7943
Devangana26 <devanganabarua@gmail.com>
2023-08-06 18:19:16 +0530

    added assets and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assistant/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Assistant',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Text('hello'),
+      home: const HomePage(),
     );
   }
 }

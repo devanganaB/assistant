7e8699d0246217ae481ecbfcf0df27630e96d97a
Devangana26 <devanganabarua@gmail.com>
2023-08-06 19:06:10 +0530

    before suggestions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:assistant/pallete.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,6 +27,39 @@ class _HomePageState extends State<HomePage> {
                   image: AssetImage('assets/images/assistant.png'))),
         ),
         //chat bubbles
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          margin: EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+          decoration: BoxDecoration(
+              border: Border.all(color: Pallete.borderColor),
+              borderRadius:
+                  BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              'Good Morning, what task can i do for you?',
+              style: TextStyle(
+                  color: Pallete.mainFontColor,
+                  fontSize: 22,
+                  fontFamily: 'Cera Pro'),
+            ),
+          ),
+        ),
+
+        //title before commands
+
+        Container(
+          padding: EdgeInsets.all(15),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Here are a few commands',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Cera Pro',
+                color: Pallete.mainFontColor,
+                fontSize: 16),
+          ),
+        )
       ]),
     );
   }
